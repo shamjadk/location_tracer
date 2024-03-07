e7caeba09e822f36e9180a45000f5189f51ea6cc
shamjadk <kshamjad1@gmail.com>
2024-03-07 12:00:37 +0530

    completed project

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -26,13 +26,16 @@ class HomePage extends ConsumerWidget {
             Container(
               padding: const EdgeInsets.only(bottom: 24),
               color: const Color(0xff333333),
-              child: Column(
+              child: GridView(
+                physics: const ClampingScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 500, mainAxisExtent: 65),
                 children: [
                   ButtonWIdget(
                     buttonName: 'Request Location Permission',
                     onPressed: () async {
                       if (await isLocationEnabled()) {
-                        /// Check if the app has permission
                         if (await requestLocationPermission()) {
                           log('Location permission granted');
                         } else {
@@ -48,9 +51,9 @@ class HomePage extends ConsumerWidget {
                     buttonName: 'Request Notification Permission',
                     onPressed: () {
                       {
-                        log("Hellow");
                         AwesomeNotifications()
                             .requestPermissionToSendNotifications();
+                        log("Notification permission granted");
                       }
                     },
                     buttonColor: const Color(0xFFf2c94c),
@@ -120,7 +123,7 @@ class HomePage extends ConsumerWidget {
                           id: 11,
                           channelKey: 'location-update',
                           actionType: ActionType.Default,
-                          title: 'Location update stoped',
+                          title: 'Location updating stoped',
                         ),
                       );
                     },
@@ -129,7 +132,57 @@ class HomePage extends ConsumerWidget {
                 ],
               ),
             ),
-            
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 500, mainAxisExtent: 90),
+                physics: const ClampingScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: ref.watch(locationProvider).locations.length,
+                itemBuilder: (context, index) {
+                  final data = ref.watch(locationProvider).locations[index];
+                  return Container(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    padding: const EdgeInsets.only(
+                        left: 8, right: 56, top: 8, bottom: 8),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: const Color(0xFFf2f2f2),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Request ${index + 1}",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Lat :${data.latitude.toStringAsFixed(3)}",
+                            ),
+                            Text(
+                              "Lng :${data.longitude.toStringAsFixed(3)}",
+                            ),
+                            Text(
+                              "Speed : ${data.speed.toStringAsFixed(0)}m",
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
